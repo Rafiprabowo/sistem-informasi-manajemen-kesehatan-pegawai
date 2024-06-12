5ac1312820134f3b941991069a1dce5d817882ce
ZhubairAbhel <100175543+ZhubairAbhel@users.noreply.github.com>
2024-06-12 13:48:23 +0700

    update session

--- resources/views/content/kategori-obat/create.blade.php
+++ resources/views/content/kategori-obat/create.blade.php
@@ -13,6 +13,22 @@
 @endsection
 @section('content')
     <div class="col-lg-8">
+        @if(session()->has('success'))
+            <div class="alert alert-success alert-dismissible" role="alert">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                         class="icon icon-tabler icons-tabler-outline icon-tabler-check">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M5 12l5 5l10 -10"/>
+                    </svg>
+                </div>
+                <div>
+                    {{session('success')}}
+                </div>
+                <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+            </div>
+        @endif
         <form class="card" action="{{route('kategori-obat.store')}}" method="post">
             @csrf
             <div class="card-header">
